9c6a6d39065d4a3d9ed6d0165ecfc7023049e25c
sahil thakur <sahilthakurxyz@gmail.com>
2024-04-28 22:45:03 +0530

    initial comment

--- backend/app.js
+++ backend/app.js
@@ -21,8 +21,8 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    // origin: ["http://localhost:3000"],
-    origin: ["https://your-personal-shop.vercel.app"],
+    origin: ["http://localhost:3000"],
+    // origin: ["https://your-personal-shop.vercel.app"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
     optionsSuccessStatus: 204,
